d21c4c9a57e275ec3b8a06aaa42b477722c91de0
yashrahmed <yashrahmed@gmail.com>
2020-11-08 19:47:17 -0600

    trying out mechanically compensated straight line driving

--- sketches/on_board/slave_augv_motor_ctrl_sketch/slave_augv_motor_ctrl_sketch.ino
+++ sketches/on_board/slave_augv_motor_ctrl_sketch/slave_augv_motor_ctrl_sketch.ino
@@ -7,6 +7,7 @@
 AccelStepper leftStepper = AccelStepper(AccelStepper::FULL4WIRE, 10, 11, 12, 13);
 AccelStepper rightStepper = AccelStepper(AccelStepper::FULL4WIRE, 6, 7, 8, 9);
 
+const float left_wheel_scale_factor = 1.0023;
 const float max_motor_speed = 5000;
 const float motor_steps_per_rotation = 2048; // @TODO - have a multiplier for differing valuefor left and right wheel.
 float left_step_speed = 0, left_ros_speed = 0, right_step_speed = 0, right_ros_speed = 0, left_ros_angle = 0, right_ros_angle = 0;
@@ -30,19 +31,20 @@ float write_float_to_bytes(float x) {
   }
 }
 
-// @TODO - change to incorporate different left and right wheel sizes.
-float ros_speed_to_step_speed(float ros_speed) {
-  return (ros_speed / (2 * PI)) * motor_steps_per_rotation;
+// parameter k is used to incorporate different left and right wheel size.
+// k = 1 for right wheel and 1/left_wheel_scale_factor for the other.
+float ros_speed_to_step_speed(float ros_speed, float k) {
+  return ((ros_speed / k) / (2 * PI)) * motor_steps_per_rotation;
 }
-// @TODO - change to incorporate different left and right wheel sizes.
-// @TODO - convert back to the reference distance that ROS thinks the wheel has traversed.
-float steps_to_ros_angle(long steps) {
-  return 2 * PI * (steps / motor_steps_per_rotation);
+// parameter k is used to incorporate different left and right wheel size.
+// k = 1 for right wheel and 1/left_wheel_scale_factor for the other.
+float steps_to_ros_angle(long steps, float k) {
+  return 2 * PI * (steps / motor_steps_per_rotation) * k;
 }
 
 void set_motor_speed_on_cmd() {
-  left_step_speed = ros_speed_to_step_speed(left_ros_speed);
-  right_step_speed = ros_speed_to_step_speed(right_ros_speed);
+  left_step_speed = ros_speed_to_step_speed(left_ros_speed, left_wheel_scale_factor);
+  right_step_speed = ros_speed_to_step_speed(right_ros_speed, 1.0);
   // -1 direction multiplier is applied as the motors are mirrored relative to each other.
   // This makes the positive direction of one the oppsite of the other.
   leftStepper.setSpeed(-1 * left_step_speed);
@@ -58,8 +60,8 @@ void receiveEvent(int bytes) {
 void requestEvent() {
   write_float_to_bytes(left_ros_speed);
   write_float_to_bytes(right_ros_speed);
-  write_float_to_bytes(-steps_to_ros_angle(leftStepper.currentPosition()));
-  write_float_to_bytes(steps_to_ros_angle(rightStepper.currentPosition()));
+  write_float_to_bytes(-steps_to_ros_angle(leftStepper.currentPosition(), left_wheel_scale_factor));
+  write_float_to_bytes(steps_to_ros_angle(rightStepper.currentPosition(), 1.0));
 }
 
 void setup() {
