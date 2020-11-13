95bf54fdb601effc078dd4def06ecdf39fcb1c89
yashrahmed <yashrahmed@gmail.com>
2020-11-12 21:51:11 -0600

    prototype short term drive planner

--- src/augv_navigation/scripts/local-drive-planner.py
+++ src/augv_navigation/scripts/local-drive-planner.py
@@ -1,30 +1,43 @@
 import sys
 import time
-from math import pi
+from math import pi, pow, sqrt
 
 import rospy
 from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
 from sensor_msgs.msg import Imu
 from std_msgs.msg import String
 from tf_conversions import transformations as txms
 
 IMU_INPUT_TOPIC = '/sensor/imu'
+ODOM_INPUT_TOPIC = '/sensor/odom'
 OUTPUT_TOPIC = '/mobile_base_controller/cmd_vel'
 DEFAULT_THETA_Z_TOLERANCE = 0.005
+DEFAULT_POS_TOLERANCE = 0.01
 
-set_point = pi / 2
+set_point = {
+    'az': pi / 2,
+    'dist': 0.5,
+    'y': 0.0
+}
 
 current_state = {
     'az': 0.0,
     'x': 0.0,
-    'y': 0.0
+    'y': 0.0,
+    'dist' : 0.0
 }
 
 
-def create_and_run_plan(publisher, angle_tgt, angle_tolerance):
+def create_and_run_plan(publisher, tgt_state, pos_tolerance, angle_tolerance):
     out_msg = Twist()
-    diff = angle_tgt - current_state['az']
-    out_msg.angular.z = diff if abs(diff) >= angle_tolerance else 0
+    az_diff = tgt_state['az'] - current_state['az']
+    pos_diff = set_point['dist'] - current_state['dist']
+    print(pos_diff)
+    if abs(az_diff) >= angle_tolerance:
+        out_msg.angular.z = az_diff
+    elif pos_diff >= pos_tolerance:
+        out_msg.linear.x = pos_diff
     publisher.publish(out_msg)
 
 
@@ -37,23 +50,40 @@ def update_imu_state(imu_message):
     current_state['az'] = az
 
 
+def update_odom_state(odom_message):
+    msg_position = odom_message.pose.pose.position
+    current_state['dist'] += sqrt(pow(current_state['x'] - msg_position.x, 2)
+                    + pow(current_state['y'] - msg_position.y, 2))
+    current_state['x'] = msg_position.x
+    current_state['y'] = msg_position.y
+
+
 def start_node():
     rospy.init_node('custom_local_planner', anonymous=True)
-    # input_topic = rospy.get_param(
-    #     f'{rospy.get_name()}/input_topic', INPUT_TOPIC)
+    # imu_input_topic = rospy.get_param(
+    #     f'{rospy.get_name()}/imu_input_topic', IMU_INPUT_TOPIC)
+    # odom_input_topic = rospy.get_param(
+    #     f'{rospy.get_name()}/odom_input_topic', ODOM_INPUT_TOPIC)
     # output_topic = rospy.get_param(
     #     f'{rospy.get_name()}/output_topic', OUTPUT_TOPIC)
     # theta_z_tolerance = rospy.get_param(
     #     f'{rospy.get_name()}/theta_z_tolerance', DEFAULT_THETA_Z_TOLERANCE)
-    input_topic = IMU_INPUT_TOPIC
+    # xy_tolerance = rospy.get_param(
+    #     f'{rospy.get_name()}/xy_tolerance', DEFAULT_POS_TOLERANCE)
+    imu_input_topic = IMU_INPUT_TOPIC
+    odom_input_topic = ODOM_INPUT_TOPIC
     output_topic = OUTPUT_TOPIC
     theta_z_tolerance = DEFAULT_THETA_Z_TOLERANCE
+    xy_tolerance = DEFAULT_POS_TOLERANCE
     publisher = rospy.Publisher(output_topic, Twist, queue_size=1000)
-    rospy.Subscriber(input_topic, Imu,
+    rospy.Subscriber(imu_input_topic, Imu,
                      callback=update_imu_state)
+    rospy.Subscriber(odom_input_topic, Odometry,
+                     callback=update_odom_state)
     rospy.Timer(rospy.Duration(0.05),
                 callback=lambda _: create_and_run_plan(publisher,
                                                        set_point,
+                                                       xy_tolerance,
                                                        theta_z_tolerance))
     rospy.spin()
 
