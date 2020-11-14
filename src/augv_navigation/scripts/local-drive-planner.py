9e61fd8e65c3f8853be25e87090f203598544e32
yashrahmed <yashrahmed@gmail.com>
2020-11-13 21:13:13 -0600

    bug fixes to allow turning in the correct direction + set point input via topic

--- src/augv_navigation/scripts/local-drive-planner.py
+++ src/augv_navigation/scripts/local-drive-planner.py
@@ -1,6 +1,7 @@
 import sys
 import time
 from math import pi, pow, sqrt
+import json
 
 import rospy
 from geometry_msgs.msg import Twist
@@ -12,36 +13,58 @@ from tf_conversions import transformations as txms
 IMU_INPUT_TOPIC = '/sensor/imu'
 ODOM_INPUT_TOPIC = '/sensor/odom'
 OUTPUT_TOPIC = '/mobile_base_controller/cmd_vel'
+COMMAND_TOPIC = '/set_point'
+
 DEFAULT_THETA_Z_TOLERANCE = 0.005
 DEFAULT_POS_TOLERANCE = 0.01
 DEFAULT_DRIVE_MODE_THETA_Z_THRESHOLD = 0.044
 
 set_point = {
-    # 'az': pi / 2,
     'az': 0.0,
-    'dist': 0.5,
-    'y': 0.0
+    'dist': 0.0,
 }
 
 current_state = {
     'az': 0.0,
     'x': 0.0,
     'y': 0.0,
-    'dist' : 0.0
+    'dist': 0.0
 }
 
 
 def create_and_run_plan(publisher, tgt_state, pos_tolerance, angle_tolerance):
     out_msg = Twist()
-    az_diff = tgt_state['az'] - current_state['az']
+    az_diff = compute_turn(tgt_state['az'], current_state['az'])
+    print(az_diff)
     pos_diff = set_point['dist'] - current_state['dist']
-    print(pos_diff)
     if abs(az_diff) >= DEFAULT_DRIVE_MODE_THETA_Z_THRESHOLD:
         out_msg.angular.z = az_diff
     else:
         out_msg.linear.x = pos_diff if abs(pos_diff) >= pos_tolerance else 0.0
         out_msg.angular.z = az_diff if abs(az_diff) >= angle_tolerance else 0.0
+
+    # Check if goal reached and then reset dist
+    # Required as distance is relative.
+    if abs(pos_diff) <= pos_tolerance and abs(az_diff) <= angle_tolerance:
+        current_state['dist'] = 0.0
+        set_point['dist'] = 0.0
+
     publisher.publish(out_msg)
+
+
+def compute_turn(a1, a2):
+    diff = a1 - a2
+    delta_z = normalize_angle(diff)
+    direction = 1 if delta_z < pi else -1
+    return direction * abs(denormalize_angle(diff))
+
+
+def denormalize_angle(a):
+    return a if a <= pi else a - 2*pi
+
+
+def normalize_angle(a):
+    return a if a >= 0 else a + 2*pi
 
 
 def update_imu_state(imu_message):
@@ -56,9 +79,20 @@ def update_imu_state(imu_message):
 def update_odom_state(odom_message):
     msg_position = odom_message.pose.pose.position
     current_state['dist'] += sqrt(pow(current_state['x'] - msg_position.x, 2)
-                    + pow(current_state['y'] - msg_position.y, 2))
+                                  + pow(current_state['y'] - msg_position.y, 2))
     current_state['x'] = msg_position.x
     current_state['y'] = msg_position.y
+
+
+def update_set_point(command):
+    print(command)
+    try:
+        cmd_values = [float(v) for v in command.data.split(",")]
+        set_point['az'], set_point['dist'] = cmd_values
+        set_point['az'] = denormalize_angle(set_point['az'])
+        rospy.loginfo(f"Updated set point to {set_point}")
+    except Exception as ex:
+        rospy.logerr(f"Received illegal command {command} exception={ex}")
 
 
 def start_node():
@@ -76,9 +110,14 @@ def start_node():
     imu_input_topic = IMU_INPUT_TOPIC
     odom_input_topic = ODOM_INPUT_TOPIC
     output_topic = OUTPUT_TOPIC
+    command_topic = COMMAND_TOPIC
+
     theta_z_tolerance = DEFAULT_THETA_Z_TOLERANCE
     xy_tolerance = DEFAULT_POS_TOLERANCE
+
     publisher = rospy.Publisher(output_topic, Twist, queue_size=1000)
+    rospy.Subscriber(command_topic, String,
+                     callback=update_set_point)
     rospy.Subscriber(imu_input_topic, Imu,
                      callback=update_imu_state)
     rospy.Subscriber(odom_input_topic, Odometry,
