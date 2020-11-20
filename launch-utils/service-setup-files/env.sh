f9439d098bda754f7c8ff67444f496ff9c3111d5
yashrahmed <yashrahmed@gmail.com>
2020-11-20 02:21:47 -0600

    working version of the launch files

--- launch-utils/service-setup-files/env.sh
+++ launch-utils/service-setup-files/env.sh
@@ -4,5 +4,5 @@
 # Taken from https://blog.roverrobotics.com/how-to-run-ros-on-startup-bootup/
 # Copy this file to /etc/ros/env.sh
 
-export ROS_HOSTNAME=$(hostname).localexport;
+export ROS_HOSTNAME=$(hostname);
 export ROS_MASTER_URI=http://$ROS_HOSTNAME:11311
