892de7e650b554b7aab4a94b346f9d84f0499347
yashrahmed <yashrahmed@gmail.com>
2020-11-19 20:41:08 -0600

    add hashbang to scripts

--- launch-utils/on-board-launcher.sh
+++ launch-utils/on-board-launcher.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
 IP_ADD=$(python3 ./launch-utils/get-ip.py)
-
+echo "ROBOT IP ${IP_ADD}"
 export ROS_MASTER_URI=http://${IP_ADD}:11311
 export ROS_HOSTNAME="${IP_ADD}"
 export ROS_IP="${IP_ADD}"
--- launch-utils/on-master-setup.sh
+++ launch-utils/on-master-setup.sh
@@ -1,4 +1,7 @@
+#!/bin/bash
+
 IP_ADD=$(python3 ./launch-utils/get-ip.py)
+echo "SYSTEM IP = ${IP_ADD}"
 echo "EXPORT variable ROS_MASTER_URI to complete setup."
 
 export ROS_HOSTNAME="${IP_ADD}"
