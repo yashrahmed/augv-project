642d90fccf0a95ea03807b7118e0f3722ba6c850
yashrahmed <yashrahmed@gmail.com>
2020-11-20 01:14:39 -0600

    specifying full paths for systemd setup

--- launch-utils/setup-launch-service.sh
+++ launch-utils/setup-launch-service.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo cp ./service-setup-files/env.sh /etc/ros/env.sh
-sudo cp ./service-setup-files/launch-nodes /usr/sbin/launch-nodes
-sudo cp ./service-setup-files/roscore.service /etc/systemd/system/roscore.service
-sudo cp ./service-setup-files/roslaunch.service /etc/systemd/system/roslaunch.service
+sudo cp "$(pwd)/launch-utils/service-setup-files/env.sh" /etc/ros/env.sh
+sudo cp "$(pwd)/launch-utils/service-setup-files/launch-nodes" /usr/sbin/launch-nodes
+sudo cp "$(pwd)/launch-utils/service-setup-files/roscore.service" /etc/systemd/system/roscore.service
+sudo cp "$(pwd)/launch-utils/service-setup-files/roslaunch.service" /etc/systemd/system/roslaunch.service
 
 sudo systemctl enable roscore.service
 sudo systemctl enable roslaunch.service
