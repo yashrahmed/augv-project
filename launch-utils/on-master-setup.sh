3fbbdc7762367b6bcc5dfaeb020d395eb59ce920
yashrahmed <yashrahmed@gmail.com>
2020-11-20 20:30:32 -0600

    Temporarily adding ROS_MASTER_URI to setup

--- launch-utils/on-master-setup.sh
+++ launch-utils/on-master-setup.sh
@@ -4,5 +4,6 @@ IP_ADD=$(python3 ./launch-utils/get-ip.py)
 echo "SYSTEM IP = ${IP_ADD}"
 echo "EXPORT variable ROS_MASTER_URI to complete setup."
 
+export ROS_MASTER_URI=http://10.0.0.115:11311 # @toDo - This is temporary
 export ROS_HOSTNAME="${IP_ADD}"
 export ROS_IP="${IP_ADD}"
